f392e4a18d21a6662cce776608d651304d3655d3
ocajulian <julian@ocampo.fr>
2026-01-31 12:52:06 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -8,5 +8,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ reply: "Missing message" });
   }
 
-  return res.status(200).json({ reply: `OK ✅ Recibí: ${message}` });
+  return res.status(200).json({
+    reply: `OK ✅ Recibí: ${message}`,
+  });
 }
